2edf5d1dd24c42bebd07d4c0471d89d606331753
Сергей Колыхалов <serg_wk75@mail.ru>
2024-07-27 05:27:34 +0300

    27.07

---
+++ helllo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void Print(const string words ) {
+   cout<<words<<endl;
+    }
+
+
+int main() {
+    string words1;
+    cin>>words1;
+    Print (words1);
+    return 0;
+}
