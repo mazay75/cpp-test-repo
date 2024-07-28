cee75d7567e5a68ab4011539864cb827a413065c
Сергей Колыхалов <serg_wk75@mail.ru>
2024-07-28 23:18:01 +0300

    28.07

--- test lambda.cpp
+++ test lambda.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int main() {
     
-    // Лексикографически сравниваем векторы чисел
-    vector<int> v1 = {3, 14, 15, 92};
+  /**/  // Лексикографически сравниваем векторы чисел
+  /*  vector<int> v1 = {3, 14, 15, 92};
     vector<int> v2 = {3, 14, 24};
     bool v1_precedes_v2 = lexicographical_compare(
         v1.begin(), v1.end(), // Первый диапазон
@@ -30,4 +30,18 @@ cout<<v1_precedes_v2<<endl;
     );
     // Ожидается, что s1_precedes_s2 == false
     cout<<s1_precedes_s2<<endl;
+*/
+     string s3 = "cat"s;
+    string s4 = "Zebra"s;
+
+    bool s3_precedes_s4  = lexicographical_compare(
+        s3.begin(), s3.end(), // Первый диапазон
+        s4.begin(), s4.end(), // Второй диапазон
+        [](char left, char right) {
+            // Сравниваем символы left и right без учёта их регистра.
+            // Возвращаем true, если left предшествует right.
+        }
+    );
+    // Ожидается, что s1_precedes_s2 == true 
+    cout<<s3_precedes_s4<<endl;
 } 
