ab9fbeec8310608d5af6e605f91b8eed0cb67722
Сергей Колыхалов <serg_wk75@mail.ru>
2024-08-11 22:29:36 +0300

    11.08

--- test.cpp
+++ test.cpp
@@ -7,10 +7,17 @@
 #include <cctype> // содержит функцию tolower.
 #include <numeric>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
-int main(){
-const vector<int> v = {1, 3, 5, 2, 6, 7, 10, 2, 3};
-cout << count_if(begin(v), end(v), [](int x) { return x > 5; }) << endl; 
-}
+
+int main() {
+    map<string, int> legs_count = {{"rabbit"s, 4}, {"dog"s, 4}, {"chicken"s, 2}};
+    const set<string> names = {"dog"s, "cat"s, "elephant"s};
+    for (const string& name : names) {
+        cout << name << " - "s << legs_count[name] << endl;
+    }
+     vector<int> prices = {100, 235, 333};
+    cout << prices.at(3) << endl;
+} 
