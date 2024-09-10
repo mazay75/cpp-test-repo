f0508d0ceb876d539b4dfdbe4706eb6ed4e5cdd8
Сергей Колыхалов <serg_wk75@mail.ru>
2024-09-10 16:42:19 +0300

    10.09

--- Chislovie tipy/Ogranichennost' pamyaty i perepolnenie_v1.0.cpp
+++ Chislovie tipy/Ogranichennost' pamyaty i perepolnenie_v1.0.cpp
@@ -32,12 +32,11 @@ max
 using namespace std;
 
 int main() {
-    const auto min = numeric_limits<int64_t>::min();    // минимальное значение типа int64_t
-    const auto max = numeric_limits<uint64_t>::max(); // максимальное значение типа uint64_t
-    cout<<min<<endl;
-    cout<<max<<endl;
-    cout<<min+max<<endl;
-    cout<<min*2<<endl;
-    cout<<max*2<<endl;
-}
+    vector<int> v = {1, 4, 5};
+for (size_t k = v.size(); k > 0; --k) {
+    const size_t i = k - 1;
+    cout << v[i] << endl;
+} 
+} 
+
     
