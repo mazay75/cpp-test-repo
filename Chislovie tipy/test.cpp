780f5b41da2e36b2d0e5edec4d2579bedca805b1
Сергей Колыхалов <serg_wk75@mail.ru>
2024-09-30 01:06:21 +0300

    30,09

--- Chislovie tipy/test.cpp
+++ Chislovie tipy/test.cpp
@@ -43,7 +43,7 @@ for (string s : some_vector) {
 cout << endl;
 */
 
-vector <int> some_vector = {2, 4, 5, 6, 7, 8, 9, 10};
+/*vector <int> some_vector = {2, 4, 5, 6, 7, 8, 9, 10};
 int k = 0;
 for (int number: some_vector) {
     if (number % 2  == 0) {
@@ -51,4 +51,17 @@ for (int number: some_vector) {
     }    
 }
 cout<<k<<endl;
+*/
+vector <string> some_vector = {"one"s, "two"s, "trrrgikk"s, "three"s, "forooooooooooooo"s, "five"s} ;
+size_t longest_index = 0;
+size_t siz = some_vector[0].size();
+for (size_t current_index = 1; current_index < some_vector.size(); ++current_index ) {
+    size_t siz1 = some_vector[current_index].size();
+   if (siz1 > siz) {
+    longest_index = current_index;
+    siz = siz1;
+   }   
 }
+cout<<"Longest string \""<<some_vector[longest_index]<<"\" at index "<<longest_index<<endl;
+}
+
