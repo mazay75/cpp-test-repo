a48393077864f93015a595a4852086ca3c125c83
Сергей Колыхалов <serg_wk75@mail.ru>
2025-03-07 08:31:22 +0300

    07.03.25

--- main.cpp
+++ main.cpp
@@ -7,17 +7,65 @@
 using namespace std;
 
 
-void PrintBirthday(string name, int birth_year, int age) {
-    
-string name;
-int age;
-double weight;
+int ComputeAbs(int num) {
+    if (num < 0) {
+        num *= (-1);
+    } 
+    return num;
+}
 
-tie(name, age, weight) = animal_info;
-cout << "Пациент "s << name
-    << ", "s << age << " лет"s
-    << ", "s << weight << " кг"s << endl;
-// Пациент Василий, 5 лет, 4.1 кг
+float ComputeAbs(float num) {
+    if (num < 0) {
+        num *= (-1.0f);
+    } 
+    return num;
+}
+
+double ComputeAbs(double num) {
+    if (num < 0) {
+        num *= (-1.0);
+    } 
+    return num;
+}
+
+vector<int> ComputeAbs(vector<int> input_data) {
+    vector<int> input_data1;
+    for (int x : input_data) {
+        if (x < 0) {
+            input_data1.push_back(x *= (-1));
+        }
+            else { 
+                input_data1.push_back(x);
+            }
+        }
+        return input_data1; 
+    }
+
+
+int main() {
+
+cout << "|100| = "s << ComputeAbs(100) << endl;
+cout << "|0| = "s << ComputeAbs(0) << endl;
+cout << "|-50| = "s << ComputeAbs(-50) << endl;
+
+cout << "|3.141592f| = "s << ComputeAbs(3.141592f) << endl;
+cout << "|0.f| = "s << ComputeAbs(0.f) << endl;
+cout << "|-3.141592f| = "s << ComputeAbs(-3.141592f) << endl;
+
+cout << "|3.141592| = "s << ComputeAbs(3.141592) << endl;
+cout << "|0| = "s << ComputeAbs(0) << endl;
+cout << "|-3.141592| = "s << ComputeAbs(-3.141592) << endl;
+
+// Тестируем версию vector
+int idx = 0;
+vector input_data = {10, 20, -30, -50, 100};
+for (int x : ComputeAbs(input_data)) {
+    // Операция ++ увеличивает значение целочисленной переменной на 1.
+    // Результатом операции ++ становится значение до увеличения.
+    cout << "|"s << input_data[idx++] << "| = "s << x << endl;
+}
+
+
     return 0;
 }
->>>>>>> 5093a6142cec6a74c1467b4ab20d4079e6cef95f
+
